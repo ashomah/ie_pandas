31ef3f86ea4345b2a368d9ae01b23f23cb8776c6
geraldwal <gerald.walravens@gmail.com>
2019-04-28 18:40:49 +0200

    Tests
    Min, max, median, mean, sum and deleted tokenize

--- tests/test_mean.py
+++ tests/test_mean.py
@@ -2,13 +2,13 @@ from ie_pandas.DataFrame import DataFrame
 import pytest
 
 
-def test_count_values_in_dataframe():
+def test_calculate_mean_values_in_dataframe():
     artists_dict = {"artist_name": ["James Blake", "Björk", "Christine & The Queens", "DJ Koze", "Solange"], "age": [30, 53, 31, 47, 32], "albums": [4, 10, 2, 5, 4]}
 
     df1 = DataFrame(artists_dict)
 
-    expected_output = age: 193, albums: 25
+    expected_output = age: 38.6, albums: 5
 
-    actual_output = df1.sum()
+    actual_output = df1.mean()
 
     assert actual_output == expected_output
--- tests/test_min.py
+++ tests/test_min.py
@@ -2,13 +2,13 @@ from ie_pandas.DataFrame import DataFrame
 import pytest
 
 
-def test_count_values_in_dataframe():
+def test_min_values_dataframe():
     artists_dict = {"artist_name": ["James Blake", "Björk", "Christine & The Queens", "DJ Koze", "Solange"], "age": [30, 53, 31, 47, 32], "albums": [4, 10, 2, 5, 4]}
 
     df1 = DataFrame(artists_dict)
 
-    expected_output = age: 193, albums: 25
+    expected_output = [{'age': 30, 'albums': 2}]
 
-    actual_output = df1.sum()
+    actual_output = df1.min()
 
     assert actual_output == expected_output
