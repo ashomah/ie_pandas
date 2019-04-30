093dff3c51e26bafd548b39162f8b3d000b69a4e
geraldwal <gerald.walravens@gmail.com>
2019-04-30 17:52:33 +0200

    Corrected versions float_dict_np_array and create_df tests
    solved with adapted notation expected_output

--- tests/dtypes/float/test_float_dict_of_np_arrays.py
+++ tests/dtypes/float/test_float_dict_of_np_arrays.py
@@ -5,7 +5,7 @@ def float_input_in_dict_of_lists():
     artists_dict_of_lists = [[30.0, 53.0, 31.0, 47.0, 32.0], [4.0, 10.0, 2.0, 5.0, 4.0]]
     np_array = np.array(artists_dict_of_lists)
     dict_of_lists_df = DataFrame(np_array, colindex=['age', 'albums'])
-
+    
     expected_output = "__age  albums/n0  30.0       4.0/n1  53.0      10.0/n2  31.0       2.0/n3  47.0       5.0/n4  32.0       4.0"
 
 
--- tests/test_create_DataFrame.py
+++ tests/test_create_DataFrame.py
@@ -2,11 +2,10 @@
 from ie_pandas import DataFrame
 
 def test_create_dataframe():
-
-dic = {'col_1': [3.0, 2.0, 1.0, 0.0],
-       	'col_2': ['as', 'bs', 'cs', 'ds'],
-       	'col_3': [3, 2, 1, 0],
-       	'col_4': [True, True, False, True]}
-
-
-df=DataFrame(dic)
+	dic = {'col_1': [3.0, 2.0, 1.0], 'col_2': ['as', 'bs', 'cs'], 'col_3': [2, 1, 0], 'col_4': [True, True, False]}
+	
+	expected_output = "__col_1  col_2  col_3  col_4/n0   3.0   'as'      2   True/n1   2.0   'bs'      1   True/n2   1.0   'cs'      0  False"
+	
+	output = DataFrame(dic)
+	
+	assert output == expected_output
