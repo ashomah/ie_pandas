7b8be5493adae6eabd75ae292cd12b525bea38e9
Ashley O'Mahony <ashley.omahony@gmail.com>
2019-04-30 20:16:59 +0200

    Added tests for empty objects and single values

---
+++ tests/OK/test_no_input.py
@@ -0,0 +1,11 @@
+from ie_pandas import DataFrame
+import pytest
+
+def test_no_input():
+
+    with pytest.raises(TypeError) as exc_info:
+        DataFrame()
+
+    exception_raised = exc_info.value
+
+    assert exception_raised
