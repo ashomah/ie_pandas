d7f4697b7f922b1066172a10b601794a7805463d
geraldwal <gerald.walravens@gmail.com>
2019-05-01 02:09:57 +0200

    Progress with tests
    Main thing to be fixed
    * print of actual = except (raised according issue) for test_string_input_in_list function in test_string_list.py
    * if this works, write test_ in front of functions in .py files of bool, float and integer (9 test total)
    * Ashley includes empty tests that will raise error
    
    Tests passed so far (12)
    tests/OK/calculations/test_max.py .                                                                                                                                                                                                    [ 14%]
    tests/OK/calculations/test_median.py .                                                                                                                                                                                                 [ 21%]
    tests/OK/calculations/test_min.py .                                                                                                                                                                                                    [ 28%]
    tests/OK/calculations/test_sum.py .                                                                                                                                                                                                    [ 35%]
    tests/OK/colindex/test_colindex_dict_of_lists.py .                                                                                                                                                                                     [ 42%]
    tests/OK/colindex/test_colindex_of_list.py .                                                                                                                                                                                           [ 50%]
    tests/OK/dot_df/test_dotdf_dict_of_lists.py .                                                                                                                                                                                          [ 57%]
    tests/OK/dot_df/test_dotdf_list_of_lists.py .                                                                                                                                                                                          [ 64%]
    tests/OK/dot_df/test_dotdf_of_list.py .                                                                                                                                                                                                [ 71%]
    tests/OK/rowindex/test_rowindex_dict_of_lists.py .                                                                                                                                                                                     [ 78%]
    tests/OK/rowindex/test_rowindex_list_of_lists.py .                                                                                                                                                                                     [ 85%]
    tests/OK/rowindex/test_rowindex_of_list.py .

---
+++ tests/OK/string/test_string_list.py
@@ -0,0 +1,13 @@
+from ie_pandas import DataFrame
+import pytest
+
+def test_string_input_in_list():
+    artists_age_list = ["James Blake", "Björk", "Christine & The Queens", "DJ Koze", "Solange"]
+
+    df1 = DataFrame(artists_age_list)
+
+    expected_output = "                        0\n0             James Blake\n1                   Björk\n2  Christine & The Queens\n3                 DJ Koze\n4                 Solange"
+    
+    actual_output = df1
+
+    assert actual_output == expected_output
