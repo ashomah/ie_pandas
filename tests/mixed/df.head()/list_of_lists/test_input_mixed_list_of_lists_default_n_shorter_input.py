1f29380a2700da3ca55a92fa2ef5bc8f39ecbd70
geraldwal <gerald.walravens@gmail.com>
2019-05-05 18:52:49 +0200

    head tests for list of lists + examples inputs and outputs test
    added tests for head function with different values for n with a nparray input

--- tests/mixed/df.head()/list_of_lists/test_input_mixed_list_of_lists_default_n_shorter_input.py
+++ tests/mixed/df.head()/list_of_lists/test_input_mixed_list_of_lists_default_n_shorter_input.py
@@ -4,10 +4,10 @@ import pytest
 
 def test_input_mixed_in_list_of_lists_head():
     obj = [
-        [30.1, 53.1, 31.1, 47.1, 32.1],
-        [4, 10, 2, 5, 4],
-        ["a", "b", "c", "d", "e"],
-        [True, False, True, True, False],
+        [30.1, 53.1],
+        [4, 10],
+        ["a", "b"],
+        [True, False],
     ]
     df = DataFrame(obj)
 
@@ -17,15 +17,9 @@ def test_input_mixed_in_list_of_lists_head():
         + "0  30.1   4  a   True"
         + "\n"
         + "1  53.1  10  b  False"
-        + "\n"
-        + "2  31.1   2  c   True"
-        + "\n"
-        + "3  47.1   5  d   True"
-        + "\n"
-        + "4  32.1   4  e  False"
     )
 
-    actual_output = repr(df.head(200))
+    actual_output = repr(df.head())
 
     assert actual_output == expected_output
 
