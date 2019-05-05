5103b3f64f09482c08775868ae3261f669cfc4f7
geraldwal <gerald.walravens@gmail.com>
2019-05-05 18:25:17 +0200

    head tests for list + examples inputs and outputs test
    added tests for head function with different values for n with a list input

---
+++ tests/mixed/df.head()/list/test_input_mixed_list_default_n.py
@@ -0,0 +1,18 @@
+from ie_pandas import DataFrame
+import pytest
+
+
+def test_input_mixed_in_list_head():
+    obj = [30, 53.0, "31", True, 32]
+    df = DataFrame(
+        obj, rowindex=["AGE"], colindex=["A", "B", "C", "D", "E"], axis=1
+    )
+
+    expected_output = (
+        "      A     B   C     D   E" + "\n" + "AGE  30  53.0  31  True  32"
+    )
+
+    actual_output = repr(df.head())
+
+    assert actual_output == expected_output
+
